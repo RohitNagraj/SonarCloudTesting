506da3eddb634dbb9edfac1d0215d33cb487faf8
Rohit Nagraj <41142333+RohitNagraj@users.noreply.github.com>
2021-01-03 12:54:11 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,5 +1,28 @@
-from light import Light
 import time
+
+class Light:
+
+    GREEN_COUNT = 0
+
+    def __init__(self):
+        self.state = 'red'
+
+    def red(self):
+        self.state = 'red'
+
+    def amber(self):
+        self.state = 'amber'
+        self.GREEN_COUNT -= 1
+
+    def green(self):
+        if self.GREEN_COUNT == 0:
+            self.state = 'green'
+            self.GREEN_COUNT += 1
+        else:
+            print('ERROR')
+
+    def get_state(self):
+        return self.state
 
 mode = int(input("Choose a mode: \n1. Auto\n2. Manual"))
 
